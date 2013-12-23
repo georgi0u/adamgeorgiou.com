8376962deb2893e5cc09394d695603e4b01f1051
Adam Georgiou <me@adamgeorgiou.com>
2013-12-23 04:47:58 +0000

    add last fm support so homepage is populated with the latests tracks I've listened too.

--- js/master.js
+++ js/master.js
@@ -74,8 +74,16 @@ function writeBooksSection() {
 
     // Book List
     var bookList = $("<ul></ul>");
-    for(i in ADAMS_STUFF["books"]) {
-        var book = ADAMS_STUFF["books"][i];
+    booksSorted = ADAMS_STUFF["books"].sort(
+        function(lhs, rhs) {
+            var leftTitle = lhs["title"].toLowerCase().replace(/^the /,"");
+            var rightTitle = rhs["title"].toLowerCase().replace(/^the /,"");
+
+            return (leftTitle < rightTitle) ? -1 : 1;
+        });
+
+    for(i in booksSorted ) {
+        var book = booksSorted[i];
 
         // Title
         var toPrint = book["title"];
@@ -134,8 +142,30 @@ function writeConcertsSection() {
     section.append(concertList);
 }
 
+function writeLastFmSection() {
+    $("#latest_tracks_container").append("<h1>latest listens</h1>");
+    $.get( "api_proxy", function( data ) {
+        var trackList = data["recenttracks"]["track"];
+
+        var trackListForDom = $("<ul></ul>");
+        
+        for(i in trackList) {
+            var track = trackList[i];
+            var toPrint = track["artist"]["#text"] + " - ";
+            toPrint += track["name"];
+
+            var url = track["url"];
+            trackListForDom.append("<li><a target='_blank' href = \"" + url + "\">" + toPrint + "</a></li>");
+        }
+
+        trackListForDom.append("<li style = 'font-style:italic;color:#aaa;'><a target='_blank' href = 'http://www.last.fm/user/georgi0u'>(see more)</a></li>");
+        $("#latest_tracks_container").append(trackListForDom);
+    });
+}
+
 $(function() {
     writeContactSection();
     writeBooksSection();
     writeConcertsSection();
+    writeLastFmSection();
 });
