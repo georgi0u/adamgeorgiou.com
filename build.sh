f130543ad01533259ba8ed29f585a4a33a82aeb6
Adam Georgiou <me@adamgeorgiou.com>
2021-09-05 20:59:10 +0000

    Adding Done

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 npx tsc -p ./ts/ && \
     npx google-closure-compiler --externs="node_modules/google-closure-compiler/contrib/externs/jquery-3.2.js" --compilation_level=ADVANCED --js="./js/**.js" --js_output_file="./compiled/compiled.js" && \
-    npx lessc --clean-css="--s1 --advanced --compatibility=ie8" ./style/style.less > ./compiled/compiled.css
+    npx lessc --clean-css="--s1 --advanced --compatibility=ie8" ./style/style.less > ./compiled/compiled.css && \
+    echo "Done."
 
